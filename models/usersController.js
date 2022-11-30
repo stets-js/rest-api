629fe59f53c52fcca1893410fdb6cba922d6dc4a
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-11-30 23:24:40 +0200

    future/avatar

--- models/usersController.js
+++ models/usersController.js
@@ -3,6 +3,9 @@ const bcrypt = require("bcrypt");
 const token = require("jsonwebtoken");
 const User = require("./usersSchema.js");
 const joi = require("./validation.js");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 class UsersController {
   async create(req, res, next) {
@@ -97,6 +100,27 @@ class UsersController {
           subscription: user.subscription,
         },
       });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async setAvatar(req, res, next) {
+    const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+    const { path: tmpUpload, originalname } = req.file;
+    console.log(req.file);
+    const { _id: id } = req.user;
+    const imageName = `${id}_${originalname}`;
+    try {
+      await Jimp.read(tmpUpload).then((image) => {
+        return image.resize(250, 250).write(tmpUpload);
+      });
+      const resultUpload = path.join(avatarsDir, imageName);
+      await fs.rename(tmpUpload, resultUpload);
+      const avatarUrl = path.join("public", "avatars", imageName);
+      await User.findByIdAndUpdate(req.user._id, { avatarUrl });
+
+      res.json({ avatarUrl });
     } catch (err) {
       console.log(err);
     }
